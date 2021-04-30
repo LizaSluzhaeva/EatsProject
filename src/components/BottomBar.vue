54296f4694c4812fe24070e9a051e9e854e9a2a2
LizaSluzhaeva <lizasluzhaeva@gmail.com>
2021-04-30 23:37:41 +0400

    feat: add modal window "help"

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
+      <ModalHelp v-if="isModalHelpVisible" @updateParent="closeModal"></ModalHelp>
       <b-navbar type="dark" variant="dark" class="d-flex justify-content-between">
         <div class="d-flex flex-column w-100">
           <b-navbar-brand href="#" class="pb-2"><img src="../../pictures/logo.svg" alt="Не удалось загрузить изображение"></b-navbar-brand>
           <b-navbar-nav class="border-top d-flex flex-row justify-content-around">
             <b-nav-item href="#">О проекте</b-nav-item>
-            <b-nav-item href="#">Помощь</b-nav-item>
+            <b-nav-item @click="isModalHelpVisible = true" href="#">Помощь</b-nav-item>
           </b-navbar-nav>
         </div>
       </b-navbar>
@@ -15,9 +16,23 @@
 <script>
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import ModalHelp from "@/components/ModalHelp";
 
 export default {
-name: "BottomBar"
+  name: "BottomBar",
+  components: {
+    ModalHelp
+  },
+  data() {
+    return {
+      isModalHelpVisible: false
+    }
+  },
+  methods: {
+    closeModal() {
+      this.isModalHelpVisible = false;
+    }
+  }
 }
 </script>
 
---
+++ src/components/ModalHelp.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="modal mb-4" tabindex="-1" style="display: block" id="closeRecipeDetailModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Помощь</h5>
+          <button @click="closeBtnClick" type="button" class="btn" data-bs-dismiss="modal" aria-label="Close">X</button>
+        </div>
+        <div class="mb-2 modal-body d-flex flex-row justify-content-between">
+          <p>Для просмотре списка рецептов нажмите на логотоп "eats" в верхнем меню. Выбрав нужный рецепт, нажмите на
+            кнопку "Добавить", для добавления соответствуюшего рецепта в нужную подборку. Посмотреть список своих подборок,
+            создать новую подборку или удалить какую-либо из существующих вы можете по нажатии на "Мои подборки" в верхнем меню.
+            Вы можете добавить свои личные рецепты по вкладке "Мои рецепты" в верхнем меню. По нажатию на логотип личного кабинета
+            в правом углу верхнего меню вы перейдете к настройке ваших данных для авторизации.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Help",
+  data() {
+    return {
+      isModalHelpVisible: false
+    }
+  },
+  methods: {
+    closeBtnClick() {
+      this.$emit('updateParent', {
+        isModalHelpVisible: this.isModalHelpVisible
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
