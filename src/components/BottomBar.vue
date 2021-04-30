bfe37547ce033f12084b63ff4983f31704d97462
LizaSluzhaeva <lizasluzhaeva@gmail.com>
2021-04-30 23:46:59 +0400

    feat: add modal window "info"

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
-      <ModalHelp v-if="isModalHelpVisible" @updateParent="closeModal"></ModalHelp>
+      <ModalHelp v-if="isModalHelpVisible" @updateParent="closeModalHelp"></ModalHelp>
+      <ModalInfo v-if="isModalInfoVisible" @updateParent="closeModalInfo"></ModalInfo>
       <b-navbar type="dark" variant="dark" class="d-flex justify-content-between">
         <div class="d-flex flex-column w-100">
           <b-navbar-brand href="#" class="pb-2"><img src="../../pictures/logo.svg" alt="Не удалось загрузить изображение"></b-navbar-brand>
           <b-navbar-nav class="border-top d-flex flex-row justify-content-around">
-            <b-nav-item href="#">О проекте</b-nav-item>
-            <b-nav-item @click="isModalHelpVisible = true" href="#">Помощь</b-nav-item>
+            <b-nav-item @click="isModalInfoVisible = true" >О проекте</b-nav-item>
+            <b-nav-item @click="isModalHelpVisible = true">Помощь</b-nav-item>
           </b-navbar-nav>
         </div>
       </b-navbar>
@@ -16,21 +17,27 @@
 <script>
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import ModalHelp from "@/components/ModalHelp";
+import ModalHelp from "@/components/ModalHelp"
+import ModalInfo from "@/components/ModalInfo";
 
 export default {
   name: "BottomBar",
   components: {
-    ModalHelp
+    ModalHelp,
+    ModalInfo
   },
   data() {
     return {
-      isModalHelpVisible: false
+      isModalHelpVisible: false,
+      isModalInfoVisible: false
     }
   },
   methods: {
-    closeModal() {
+    closeModalHelp() {
       this.isModalHelpVisible = false;
+    },
+    closeModalInfo() {
+      this.isModalInfoVisible = false;
     }
   }
 }
---
+++ src/components/ModalInfo.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="modal mb-4" tabindex="-1" style="display: block" id="closeRecipeDetailModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Информация о проекте</h5>
+          <button @click="closeBtnClick" type="button" class="btn" data-bs-dismiss="modal" aria-label="Close">X</button>
+        </div>
+        <div class="mb-2 modal-body d-flex flex-row justify-content-between">
+          <p>Наше приложение позволяет вам делать подборки из любимых рецептов, фильтровать рецепты по типам, добавлять
+            свои рецепты в общий пул рецептов и при желании делится ими с другими пользователями</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalInfo",
+  data() {
+    return {
+      isModalInfoVisible: false
+    }
+  },
+  methods: {
+    closeBtnClick() {
+      this.$emit('updateParent', {
+        isModalInfoVisible: this.isModalInfoVisible
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
